1aa0a85f07c168076c4dd23d57c9fe078a4af611
Lebron2004 <102331426+Lebron2004@users.noreply.github.com>
2024-10-11 16:27:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,33 +17,33 @@
         <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body id="page-top">
-      <!-- Navigation-->
-   	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-        <div class="container">
-        	<a class="navbar-brand" href="#page-top"><img src="assets/img/Horizon logo.jpg" alt="..." /></a>
-            	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	<!-- Naviagtion -->
+	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+   	 <div class="container">
+    	<a class="navbar-brand" href="#page-top"><img src="assets/img/Horizon logo.jpg" alt="..." /></a>
+        	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
-            	<i class="fas fa-bars ms-1"></i>
+            <i class="fas fa-bars ms-1"></i>
         	</button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
                 <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
                 <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
                 <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                <?php if (isset($_SESSION['username'])): ?>
-                    <li class="nav-item">
+               		<?php if (isset($_SESSION['username'])): ?>
+                    	<li class="nav-item">
                         <a class="nav-link" href="logout.php">Logout (<?= htmlspecialchars($_SESSION['username']) ?>)</a>
-                    </li>
-                <?php else: ?>
-                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
+                    	</li>
+               		 <?php else: ?>
+                   	 <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                    	<li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
                 <?php endif; ?>
-                </ul>
-            </div>
-        </div>
-    </nav>
+            	</ul>
+        	</div>
+    		</div>
+	</nav>
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
