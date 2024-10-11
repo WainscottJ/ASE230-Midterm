4c42b5a25c9e94aa3f6cea71d334b35adfe957dd
Lebron2004 <102331426+Lebron2004@users.noreply.github.com>
2024-10-11 16:07:59 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header('Location: index.php'); // Redirect to the home page
-exit;
+session_destroy(); // Destroy all session data
+header("Location: index.php"); // Redirect to the homepage
+exit();
 ?>
-
-
