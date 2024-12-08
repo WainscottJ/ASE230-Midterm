3ef19d30235ff2923a3936155ab3489a270b181d
Wainscottj <wainscottj7@mymail.nku.edu>
2024-12-07 19:05:23 -0500

    Changed some items in the header to have a continuous Theme among the various different pages for admin page and user page

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">My Website</a>
+    <a class="navbar-brand" href="index.php">Horizon Media </a>
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
